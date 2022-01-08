c2e575d309edc449e7a2fdeb9f4ad3c34f0b1e82
TOSHKO003 <84236467+TOSHKO003@users.noreply.github.com>
2022-01-08 20:13:20 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config.Locations = {
-    [1] = {coords = vector3(1596.78, 6619.98, 15.71), data = "loc1"} --data decides the type of return items (check config.items)
+    [1] = {coords = vector3(1596.78, 6619.98, 15.71), data = "loc1"}, --data decides the type of return items (check config.items)
     [2] = {coords = vector3(1596.78, 6619.98, 15.71), data = "loc1"}
 }
 
